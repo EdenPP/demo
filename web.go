228c2ab16fb6dad1378c02a14e93704bed71761e
EdenPP <yide.me@iCloud.com>
2016-02-19 10:24:49 +0800

    Modify file

--- web.go
+++ web.go
@@ -1,5 +1,11 @@
 package web
 
-func Weburl() {
-    return 10
+func Sqrt(x float64) float64 {
+    z := 1.0
+
+    for i := 0; i < 1000; i++ {
+        z -= (z * z - x) / (2 * x)
+    }
+
+    return z
 }
